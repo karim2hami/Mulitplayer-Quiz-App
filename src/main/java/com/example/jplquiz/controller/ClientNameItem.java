2ec5ce76c25d89a88af3e58d6783a8e552f44f82
karimtouhami <IKT360%school>
2022-02-22 15:08:15 +0100

    created client-nameItem.fxml and client-questionScreen.fxml

---
+++ src/main/java/com/example/jplquiz/controller/ClientNameItem.java
@@ -0,0 +1,11 @@
+package com.example.jplquiz.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ClientNameItem {
+
+  @FXML
+  private Label lb_nickname;
+
+}
---
+++ src/main/java/com/example/jplquiz/controller/ClientQuestionScreen.java
@@ -0,0 +1,38 @@
+package com.example.jplquiz.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ClientQuestionScreen {
+
+  @FXML
+  private Button btn_A;
+
+  @FXML
+  private Button btn_B;
+
+  @FXML
+  private Button btn_C;
+
+  @FXML
+  private Button btn_D;
+
+  @FXML
+  private ImageView img_question;
+
+  @FXML
+  private Label lb_countDown;
+
+  @FXML
+  private Label lb_playerPoints;
+
+  @FXML
+  private Label lb_question;
+
+  @FXML
+  private Label lb_questionCounter;
+
+}
+
