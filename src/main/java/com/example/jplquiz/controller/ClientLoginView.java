5fb6861731974638be7cfc08030631da56e91c5c
devin <devin.hasler@student.ffhs.ch>
2022-04-21 17:43:21 +0200

    worked on Client und ClientQuestionView.java

--- src/main/java/com/example/jplquiz/controller/ClientLoginView.java
+++ src/main/java/com/example/jplquiz/controller/ClientLoginView.java
@@ -1,17 +1,21 @@
 package com.example.jplquiz.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import members.Client;
 
-public class ClientLoginView {
+public class ClientLoginView implements Initializable {
 
   @FXML
   private Button btn_enter;
 
   @FXML
   private TextField tfd_nickname;
+
 
   public Button getBtn_enter() {
     return btn_enter;
@@ -27,5 +31,10 @@ public class ClientLoginView {
 
   public void setTfd_nickname(String tfd_nickname) {
     this.tfd_nickname.setText(tfd_nickname);
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
   }
 }
