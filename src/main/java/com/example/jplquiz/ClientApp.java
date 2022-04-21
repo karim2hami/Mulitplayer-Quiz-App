213e3a30f079ffbb0dba13c5619ebf9fd058339f
devin <devin.hasler@student.ffhs.ch>
2022-04-21 17:43:21 +0200

    worked on Client und ClientQuestionView.java

--- src/main/java/com/example/jplquiz/ClientApp.java
+++ src/main/java/com/example/jplquiz/ClientApp.java
@@ -2,15 +2,10 @@ package com.example.jplquiz;
 
 import com.example.jplquiz.controller.ClientLoginView;
 import java.io.IOException;
-import java.net.Socket;
-
-import com.example.jplquiz.controller.ClientLoginView;
-import com.example.jplquiz.controller.ClientQuestionView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import members.Client;
 import members.ClientThread;
 
 public class ClientApp extends Application {
@@ -25,29 +20,9 @@ public class ClientApp extends Application {
       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
           "client-questionView.fxml"));
       Scene scene = new Scene(fxmlLoader.load());
-      ClientQuestionView clientQuestionView = fxmlLoader.getController();
       primaryStage.setTitle("Multiplayer Quiz App");
       primaryStage.setScene(scene);
       primaryStage.show();
-
-      Socket socket = null;
-      try {
-        socket = new Socket("localhost", 1234);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
-
-      Client client = new Client(socket, "devin");
-      client.listenForQuestions();
-
-      System.out.println("hallo");
-      client.sendMessage();
-      System.out.println("hallo2");
-      Thread.sleep(100);
-      client.transferQuestions(clientQuestionView);
-//      ClientThread clientThread = new ClientThread();
-//      clientThread.execute();
 
 
 
@@ -56,8 +31,6 @@ public class ClientApp extends Application {
 
     } catch (IOException e) {
       e.printStackTrace();
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
     }
   }
 }
