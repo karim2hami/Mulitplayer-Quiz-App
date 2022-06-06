7db08170354eb5645382f7315dbf39da6dd098e6
Karim Touhami <karim.touhami19@gmail.com>
2022-06-07 00:37:20 +0200

    added javaDoc of ClientHandler

--- src/main/java/members/ClientHandler.java
+++ src/main/java/members/ClientHandler.java
@@ -8,138 +8,165 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * @author karim2hami
+ *     <p>ClientHandler: Handles the connection and communication between Server and all Clients.
+ */
 public class ClientHandler implements Runnable {
 
-    public static final List<ClientHandler> clientHandlers = new ArrayList<>();
-    private ObjectOutputStream outputStream;
+  public static final List<ClientHandler> clientHandlers = new ArrayList<>();
+  private ObjectOutputStream outputStream;
 
-    private static int index = 0;
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-    private String clientUsername;
+  private static int index = 0;
+  private Socket socket;
+  private BufferedReader bufferedReader;
+  private BufferedWriter bufferedWriter;
+  private String clientUsername;
 
-    private static final HashMap<String, Integer> namePointsMap = new HashMap<>();
+  private static final HashMap<String, Integer> namePointsMap = new HashMap<>();
 
-    private boolean isStart;
+  private boolean isStart;
 
-    private ServerClientDashboard serverClientDashboard;
+  private ServerClientDashboard serverClientDashboard;
 
-    /**
-     * @param socket <Client Handler that is responsible for communication with Client and Server
-     *               <OutputStream is wrapped with BufferWriter for sending characters and not bytes same is
-     *               <for InputStream
-     */
-    public ClientHandler(Socket socket) {
-        try {
-            this.socket = socket;
-            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.outputStream = new ObjectOutputStream(socket.getOutputStream());
-            this.clientUsername = bufferedReader.readLine();
-            clientHandlers.add(this);
-            broadcastMessage("Server: " + clientUsername + " has entered the game!");
-        } catch (IOException e) {
-            closeEverything(socket, bufferedReader, bufferedWriter);
+  /**
+   * @param socket <Client Handler that is responsible for communication with Client and Server
+   *     <OutputStream is wrapped with BufferWriter for sending characters and not bytes same is
+   *     <for InputStream
+   */
+  public ClientHandler(Socket socket) {
+    try {
+      this.socket = socket;
+      this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+      this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+      this.clientUsername = bufferedReader.readLine();
+      clientHandlers.add(this);
+      broadcastMessage("Server: " + clientUsername + " has entered the game!");
+    } catch (IOException e) {
+      closeEverything(socket, bufferedReader, bufferedWriter);
+    }
+  }
+
+  /**
+   * @author karimtouhami
+   *     <p>Listens for messages from the Client.
+   */
+  @Override
+  public void run() {
+    String messageFromClient;
+
+    while (socket.isConnected()) {
+      try {
+        messageFromClient = bufferedReader.readLine();
+        if (!isStart) {
+          serverClientDashboard.addName(messageFromClient);
         }
+        broadcastMessage(messageFromClient);
+      } catch (IOException e) {
+        closeEverything(socket, bufferedReader, bufferedWriter);
+        break;
+      }
     }
+  }
 
-    @Override // Listens for messages from the Client
-    public void run() {
-        String messageFromClient;
+  /**
+   * @author karimtouhami
+   *     <p>Sends the game rankings to all clients at the same time.
+   * @param messageToSend - message that is sent to the client
+   */
+  // Send a message to all clients at the same time
+  public void broadcastMessage(String messageToSend) {
 
-        while (socket.isConnected()) {
-            try {
-                messageFromClient = bufferedReader.readLine();
-                if (!isStart) {
-                    serverClientDashboard.addName(messageFromClient);
-                }
-                broadcastMessage(messageFromClient);
-            } catch (IOException e) {
-                closeEverything(socket, bufferedReader, bufferedWriter);
-                break;
-            }
+    System.out.println("clienthandler size" + clientHandlers.size());
+
+    System.out.println("message to send" + messageToSend + "\n");
+    for (ClientHandler clientHandler : clientHandlers) {
+      try {
+
+        if (isStart) {
+          String[] namesPointsArray = messageToSend.split(";");
+          namePointsMap.put(namesPointsArray[0], Integer.parseInt(namesPointsArray[1]));
+          System.out.println(namePointsMap);
+          index++;
+        } else if (messageToSend.equals("true")) {
+          clientHandler.setStart(true);
         }
-    }
-
-    // Send a message to all clients at the same time
-    public void broadcastMessage(String messageToSend) {
-
-        System.out.println("clienthandler size" + clientHandlers.size());
-
-        System.out.println("message to send" + messageToSend + "\n");
-        for (ClientHandler clientHandler : clientHandlers) {
-            try {
-
-                if (isStart) {
-                    String[] namesPointsArray = messageToSend.split(";");
-                    namePointsMap.put(namesPointsArray[0], Integer.parseInt(namesPointsArray[1]));
-                    System.out.println(namePointsMap);
-                    index++;
-                }else if (messageToSend.equals("true")) {
-                    clientHandler.setStart(true);
-                }
-                clientHandler.bufferedWriter.write(messageToSend);
-                // clients wait for the new line
-                clientHandler.bufferedWriter.newLine();
-                clientHandler.bufferedWriter.flush();
-                if(index == clientHandlers.size()){
-                    clientHandler.bufferedWriter.close();
-                    broadcastResultMap();
-                    System.out.println("buferred writer was closed");
-                }
-            } catch (IOException e) {
-                closeEverything(socket, bufferedReader, bufferedWriter);
-            }
+        clientHandler.bufferedWriter.write(messageToSend);
+        // clients wait for the new line
+        clientHandler.bufferedWriter.newLine();
+        clientHandler.bufferedWriter.flush();
+        if (index == clientHandlers.size()) {
+          clientHandler.bufferedWriter.close();
+          broadcastResultMap();
+          System.out.println("buferred writer was closed");
         }
+      } catch (IOException e) {
+        closeEverything(socket, bufferedReader, bufferedWriter);
+      }
     }
+  }
 
-    public void broadcastResultMap(){
+  /**
+   * @author karimtouhami
+   *     <p>Broadcasts the HashMap with all names and scores to each of the Clients ClientHandlers.
+   */
+  public void broadcastResultMap() {
 
-        for (ClientHandler clientHandler : clientHandlers) {
-            try {
-                clientHandler.outputStream.writeObject(namePointsMap);
-                clientHandler.outputStream.flush();
-                clientHandler.outputStream.close();
-            } catch (IOException e) {
-                closeEverything(socket, bufferedReader, bufferedWriter);
-            }
-        }
+    for (ClientHandler clientHandler : clientHandlers) {
+      try {
+        clientHandler.outputStream.writeObject(namePointsMap);
+        clientHandler.outputStream.flush();
+        clientHandler.outputStream.close();
+      } catch (IOException e) {
+        closeEverything(socket, bufferedReader, bufferedWriter);
+      }
     }
+  }
 
-    public void removeClientHandler() {
-        clientHandlers.remove(this);
-        broadcastMessage("Server : " + clientUsername + " has left the game!");
+  /**
+   * @author karimtouhami
+   *     <p>Removes a client from the ClientHandler List.
+   */
+  public void removeClientHandler() {
+    clientHandlers.remove(this);
+    broadcastMessage("Server : " + clientUsername + " has left the game!");
+  }
+
+  /**
+   * @author karimtouhami
+   *     <p>Closes all running sockets, connections, readers and writers.
+   * @param socket
+   * @param bufferedReader
+   * @param bufferedWriter
+   */
+  public void closeEverything(
+      Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+    removeClientHandler();
+    try {
+      if (bufferedReader != null) {
+        bufferedReader.close();
+      }
+      if (bufferedWriter != null) {
+        bufferedWriter.close();
+      }
+      if (socket != null) {
+        socket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void closeEverything(
-            Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
-        removeClientHandler();
-        try {
-            if (bufferedReader != null) {
-                bufferedReader.close();
-            }
-            if (bufferedWriter != null) {
-                bufferedWriter.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  public void setServerClientDashboard(ServerClientDashboard serverClientDashboard) {
+    this.serverClientDashboard = serverClientDashboard;
+  }
 
+  public List<ClientHandler> getClientHandlers() {
+    return clientHandlers;
+  }
 
-    public void setServerClientDashboard(ServerClientDashboard serverClientDashboard) {
-        this.serverClientDashboard = serverClientDashboard;
-    }
-
-    public List<ClientHandler> getClientHandlers() {
-        return clientHandlers;
-    }
-
-    public void setStart(boolean start) {
-        isStart = start;
-    }
+  public void setStart(boolean start) {
+    isStart = start;
+  }
 }
